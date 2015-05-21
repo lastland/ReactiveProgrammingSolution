ace5a1914871f22b6c58372d566248f3e5e14202
Yao Li <hnkfliyao@gmail.com>
2015-05-21 23:16:18 +0200

    Week 5: Actor Binary Tree. My solution.

--- actorbintree/src/main/scala/actorbintree/BinaryTreeSet.scala
+++ actorbintree/src/main/scala/actorbintree/BinaryTreeSet.scala
@@ -43,14 +43,14 @@ object BinaryTreeSet {
     * `result` is true if and only if the element is present in the tree.
     */
   case class ContainsResult(id: Int, result: Boolean) extends OperationReply
-  
+
   /** Message to signal successful completion of an insert or remove operation. */
   case class OperationFinished(id: Int) extends OperationReply
 
 }
 
 
-class BinaryTreeSet extends Actor {
+class BinaryTreeSet extends Actor with ActorLogging {
   import BinaryTreeSet._
   import BinaryTreeNode._
 
@@ -66,15 +66,39 @@ class BinaryTreeSet extends Actor {
 
   // optional
   /** Accepts `Operation` and `GC` messages. */
-  val normal: Receive = { case _ => ??? }
+  val normal: Receive = {
+    case op: Operation =>
+      log.debug(s"Emit $op to $root")
+      root ! op
+    case GC =>
+      val newRoot = createRoot
+      root ! CopyTo(newRoot)
+      log.debug(s"Enter GC Mode with $newRoot as newRoot")
+      context.become(garbageCollecting(newRoot))
+  }
 
-  // optional
   /** Handles messages while garbage collection is performed.
     * `newRoot` is the root of the new binary tree where we want to copy
     * all non-removed elements into.
     */
-  def garbageCollecting(newRoot: ActorRef): Receive = ???
-
+  def garbageCollecting(newRoot: ActorRef): Receive = {
+    case op: Operation =>
+      log.debug(s"Add operation $op to pending")
+      pendingQueue = pendingQueue enqueue op
+    case CopyFinished =>
+      log.debug("Copy is done")
+      root = newRoot
+      for (op <- pendingQueue) {
+        log.debug(s"Emit $op to $root")
+        root ! op
+      }
+      pendingQueue = Queue.empty[Operation]
+      log.debug("Enter Normal Mode")
+      context.become(normal)
+    case GC =>
+      // just ignore it
+      ()
+  }
 }
 
 object BinaryTreeNode {
@@ -89,25 +113,131 @@ object BinaryTreeNode {
   def props(elem: Int, initiallyRemoved: Boolean) = Props(classOf[BinaryTreeNode],  elem, initiallyRemoved)
 }
 
-class BinaryTreeNode(val elem: Int, initiallyRemoved: Boolean) extends Actor {
+class BinaryTreeNode(val elem: Int, initiallyRemoved: Boolean) extends Actor
+    with ActorLogging {
   import BinaryTreeNode._
   import BinaryTreeSet._
 
   var subtrees = Map[Position, ActorRef]()
   var removed = initiallyRemoved
+  var reportTo: Option[ActorRef] = None
 
   // optional
   def receive = normal
 
+  private def insertAtChild(p: Position, insert: Insert) {
+    if (subtrees contains p) {
+      subtrees(p) ! insert
+    } else {
+      val e = insert.elem
+      subtrees = subtrees + (p ->
+        context.actorOf(props(insert.elem, false), s"node$e"))
+      insert.requester ! OperationFinished(insert.id)
+    }
+  }
+
+  private def removeAtChild(p: Position, remove: Remove) {
+    if (subtrees contains p) {
+      subtrees(p) ! remove
+    } else {
+      remove.requester ! OperationFinished(remove.id)
+    }
+  }
+
+  private def containsAtChild(p: Position, contains: Contains) {
+    if (subtrees contains p) {
+      subtrees(p) ! contains
+    } else {
+      contains.requester ! ContainsResult(contains.id, false)
+    }
+  }
+
+  private def copyAtChild(p: Position, copy: CopyTo) {
+    if (subtrees contains p) {
+      subtrees(p) ! copy
+    }
+  }
+
   // optional
   /** Handles `Operation` messages and `CopyTo` requests. */
-  val normal: Receive = { case _ => ??? }
+  val normal: Receive = {
+    case Insert(requester, id, elem) =>
+      if (elem == this.elem) {
+        if (removed) removed = false
+        requester ! OperationFinished(id)
+      } else if (elem < this.elem) {
+        insertAtChild(Left, Insert(requester, id, elem))
+      } else {
+        insertAtChild(Right, Insert(requester, id, elem))
+      }
+    case Remove(requester, id, elem) =>
+      if (elem == this.elem) {
+        if (!removed) removed = true
+        requester ! OperationFinished(id)
+      } else if (elem < this.elem) {
+        removeAtChild(Left, Remove(requester, id, elem))
+      } else {
+        removeAtChild(Right, Remove(requester, id, elem))
+      }
+    case Contains(requester, id, elem) =>
+      if (elem == this.elem) {
+        requester ! ContainsResult(id, !removed)
+      } else if (elem < this.elem) {
+        containsAtChild(Left, Contains(requester, id, elem))
+      } else {
+        containsAtChild(Right, Contains(requester, id, elem))
+      }
+    case CopyTo(actorRef) =>
+      if (!removed)
+        actorRef ! Insert(self, 0, elem)
+      copyAtChild(Left, CopyTo(actorRef))
+      copyAtChild(Right, CopyTo(actorRef))
+      reportTo = Some(sender)
+      val expected = subtrees.values.toSet
+      if (expected.isEmpty && removed) {
+        stop
+      } else {
+        log.debug(s"Enter Copying Mode: $expected")
+        context.become(copying(expected, removed))
+      }
+  }
 
   // optional
   /** `expected` is the set of ActorRefs whose replies we are waiting for,
     * `insertConfirmed` tracks whether the copy of this node to the new tree has been confirmed.
     */
-  def copying(expected: Set[ActorRef], insertConfirmed: Boolean): Receive = ???
+  def copying(expected: Set[ActorRef], insertConfirmed: Boolean): Receive = {
+    case CopyFinished if (insertConfirmed) =>
+      log.debug(s"$sender finished")
+      if (expected.dropWhile(_ == sender).isEmpty) {
+          stop
+      } else {
+        val e = expected - sender
+        log.debug(s"Enter Copying Mode: $e, $insertConfirmed")
+        context.become(copying(e, insertConfirmed))
+      }
+    case CopyFinished if (!insertConfirmed) =>
+      log.debug(s"$sender finished")
+      val e = expected - sender
+      log.debug(s"Enter Copying Mode: $e, $insertConfirmed")
+      context.become(copying(e, insertConfirmed))
+    case OperationFinished(_) =>
+      log.debug("Insertion finished.")
+      if (expected.isEmpty) {
+        stop
+      } else {
+        log.debug(s"Enter Copying Mode: $expected, true")
+        context.become(copying(expected, true))
+      }
+  }
 
-
+  def stop() {
+    log.debug("successfully finished copying, shutting down")
+    reportTo match {
+      case Some(a) =>
+        a ! CopyFinished
+      case _ => ()
+    }
+    context.stop(self)
+  }
 }
